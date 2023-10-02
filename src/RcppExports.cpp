0503df833fbdf5937f726a4ae8f845f33d771a74
geunseoplee <55769729+geunseoplee@users.noreply.github.com>
2023-10-02 22:54:25 +0900

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -8,12 +8,12 @@ using namespace Rcpp;
 
 // rpg3
 List rpg3(int num, double z);
-RcppExport List rpg3_rpg3(SEXP nSEXP, SEXP zSEXP) {
+RcppExport SEXP rpg3_rpg3(SEXP nSEXP, SEXP zSEXP) {
 BEGIN_RCPP
         Rcpp::traits::input_parameter< int >::type n(nSEXP );
         Rcpp::traits::input_parameter< double >::type z(zSEXP );
 
-        Rcpp::RObject rcpp_result_gen; = Rcpp::wrap(rpg3(n, z));
+        Rcpp::RObject rcpp_result_gen = Rcpp::wrap(rpg3(n, z));
         return rcpp_result_gen;
 END_RCPP
 }
