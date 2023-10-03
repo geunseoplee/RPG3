e23dfb91be81c69a5225c4aae3cb0c2c982b530c
geunseoplee <55769729+geunseoplee@users.noreply.github.com>
2023-10-03 22:33:00 +0900

    Update helloPG.cpp

--- src/helloPG.cpp
+++ src/helloPG.cpp
@@ -69,44 +69,5 @@ SEXP rpg2(int n, double z) {
 		rpg_vec(i) = rpg_i;
 	}
 	
-	return Rcpp::List::Create(rpg_vec);
+	return Rcpp::List::Create(Rcpp::Named("draws")=rpg_vec);
 }
-
-/*
-#include "RcppArmadillo.h"
-#include <R_ext/Utils.h>
-#include <iostream>
-#include <exception>
-#include "RNG.h"
-#include "PolyaGamma.h"
-// Rcpp::depends(RcppArmadillo)
-
-using namespace Rcpp;
-using namespace arma;
-
-double rpg2(double scale)
-{
-  RNG r;
-  PolyaGamma pg;
-  #ifdef USE_R
-    GetRNGstate();
-  #endif
-  double result = pg.draw(1, scale, r);
- #ifdef USE_R
-    PutRNGstate();
-  #endif
-  return result;
-}
-
-// [[Rcpp::export]]
-List rpg3(int num, double z) {
-
-  vec rpg_vec(num);
-  for(int i = 0;i<num;i++)
-  {
-	rpg_vec(i) = rpg2(z);	
-  }
-
-  return Rcpp::List::create(rpg_vec);
-}
-*/
