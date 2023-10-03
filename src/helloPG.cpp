34bf659ee4d2ceaee63895569b42e32c0e09de01
geunseoplee <55769729+geunseoplee@users.noreply.github.com>
2023-10-03 22:26:23 +0900

    Update helloPG.cpp

--- src/helloPG.cpp
+++ src/helloPG.cpp
@@ -28,6 +28,24 @@ colvec rpg(colvec shape, colvec scale) {
   return result;
 }
 
+double rpg_single(double scale) {
+// C++-only interface to PolyaGamma class
+// draws random PG variates from arma::vectors of n's and psi's
+  RNG r;
+  PolyaGamma pg;
+#ifdef USE_R
+  GetRNGstate();
+#endif
+  int d = shape.n_elem;
+  colvec result(1);
+  result[0] = pg.draw(1, scale, r);
+  }
+#ifdef USE_R
+  PutRNGstate();
+#endif
+  return result;
+}
+
 // [[Rcpp::export]]
 SEXP helloPG(int n, double z) {
   // returns n draws from PG(1,z)
@@ -44,17 +62,15 @@ SEXP helloPG(int n, double z) {
 
 // [[Rcpp::export]]
 SEXP rpg2(int n, double z) {
-  // returns n draws from PG(1,z)
-  n = 2;
-  colvec pgscale(n, fill::ones);
-  colvec pgshape(n);
-  for(int i=0; i<n; i++) {
-    pgshape[i] = z;
-  }
-  colvec out = rpg(pgscale, pgshape);
 
-  return Rcpp::List::create(Rcpp::Named("draws")=out
-			    );
+	vec rpg_vec(n);
+	for(int i = 0;i<n;i++)
+	{
+		double rpg_i = rpg_single(1, z);
+		rpg_vec(i) = rpg_i;
+	}
+	
+	return Rcpp::List::Create(rpg_vec);
 }
 
 /*
